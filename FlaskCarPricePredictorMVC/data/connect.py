19d387db74b8d8f11cdabf15a0da952c5ff9b185
gtadayukey <gtadayukey@gmail.com>
2024-04-13 06:03:06 -0300

    add peewee orm for postgres

--- FlaskCarPricePredictorMVC/data/connect.py
+++ FlaskCarPricePredictorMVC/data/connect.py
@@ -1,2 +1,25 @@
+import datetime
+from peewee import *
+
+db = PostgresqlDatabase('flaskcarpricepredictormvcdb', user='postgres', password='12341234',
+                        host='localhost', port=5432)
 
 
+class User(Model):
+    __tablename__ = 'users'
+    first_name = CharField()
+    last_name = CharField()
+    register_date = DateTimeField(default=datetime.UTC)
+
+    class Meta:
+        database = db
+
+
+class Login(Model):
+    __tablename__ = 'login'
+    user_id = ForeignKeyField(User, backref="users")
+    email = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = db
